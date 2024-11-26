00d67d98efbc4c466b9355860a1c141325d301ae
LuisCruzA <148837161+LuisCruzA@users.noreply.github.com>
2024-11-26 12:48:11 -0600

    Add files via upload

---
+++ proba.ordi.R
@@ -0,0 +1,23 @@
+## app.R ##
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "DashBoard Col. Satelite"),
+  dashboardSidebar( # Corrige el nombre a "dashboardSidebar"
+    sidebarMenu(
+      menuItem("Mapas", tabName = "histogramas_mapas", icon = icon("map")),
+      menuItem("Histogramas", tabName = "histo", icon= icon("chart-bar"))
+    )
+  ),
+  dashboardBody(
+    tabItem(tabName = "histogramas_mapas"
+      
+    ),
+    tabItem(tabName = "histo")
+  )
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
