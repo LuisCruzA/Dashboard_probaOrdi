98dcefc14bbde4f3adccc2f1cbd2bc2dd4887a5d
luis <langel18.1818@gmail.com>
2024-11-28 10:20:30 -0600

    se agrego opciones de despliegue, falta mejorarlo pero ya tenenemos un avance

--- proba.ordi.R
+++ proba.ordi.R
@@ -2,23 +2,70 @@
 ##prueba git##
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
 
 ui <- dashboardPage(
   dashboardHeader(title = "DashBoard Col. Satelite"),
-  dashboardSidebar( # Corrige el nombre a "dashboardSidebar"
+  dashboardSidebar( 
     sidebarMenu(
       menuItem("Mapas", tabName = "histogramas_mapas", icon = icon("map")),
-      menuItem("Histogramas", tabName = "histo", icon= icon("chart-bar"))
+      
+      
+      # Menú desplegable para escoger el tipo de mapa
+      selectInput("map_option", "Elige el tipo de mapa:",
+                  choices = c("Mapa de Calor", "Mapa de Puntos", "Mapa de Líneas"),
+                  selected = "Mapa de Calor"),
+      
+      
+      menuItem("Graficas", tabName = "histo", icon= icon("chart-bar")),
+      
+      
+      
+      # Menú de radio buttons para escoger el tipo de gráfico
+      selectInput("graph_option", "Elige el tipo de gráfico:",
+                   choices = c("Histograma", "Gráfico de Barras", "Gráfico de Dispersión"),
+                   selected = "Histograma")
+      
     )
   ),
   dashboardBody(
-    tabItem(tabName = "histogramas_mapas"
-      
+    # Pestaña de Mapas
+    tabItem(tabName = "histogramas_mapas",
+            box(
+              title = "Opciones de Mapa",
+              width = 12,
+              solidHeader = TRUE,
+              status = "primary",
+              # texto dde salida
+              textOutput("Mapa_Seleccionado")
+            )
     ),
-    tabItem(tabName = "histo")
+    
+    # Pestaña de Gráficas
+    tabItem(tabName = "histo",
+            box(
+              title = "Opciones de Gráficas",
+              width = 12,
+              solidHeader = TRUE,
+              status = "primary",
+              # texto de salida
+              textOutput("Grafica_Seleccionada")
+            )
+    )
   )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) {
+  #se muestra la opcion seleccionada para mapas
+  output$Mapa_Seleccionado <- renderText({
+    paste("Tipo de Mapa Seleccionado: ", input$map_option)
+    
+  })
+  
+  # Mostrar la opción seleccionada para gráficos
+  output$Grafica_Seleccionada <- renderText({
+    paste("Tipo de gráfico seleccionado:", input$graph_option)
+  })
+}
 
 shinyApp(ui, server)
