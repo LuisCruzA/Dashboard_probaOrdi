a1bca7023164ec2a6f872e3427dc418077a79935
luis <langel18.1818@gmail.com>
2024-12-01 11:55:15 -0600

    se conecto el mapa con los datos del excel

--- proba.ordi.R
+++ proba.ordi.R
@@ -6,15 +6,25 @@ library(shinydashboard)
 library(ggplot2)
 library(sf)
 library(leaflet)
+library(dplyr)
+library(readxl)
 
 
 shp_url <- "mapas/colonia_Satelite 1.shp"
 
+datoscol <- "excel/gdatosdashboard-2.xlsx"
+
 # Leer el shapefile 
 mapa <- st_read(shp_url)
+poblacion <- read_excel(datoscol)
 
 # Ver los primeros registros del shapefile
 head(mapa)
+
+#unir datos del shp con los datos del excel usando cvgeo
+datos <- poblacion %>% rename(ID = "CVEGEO")
+colonia <- mapa %>% rename(ID = "CVEGEO")
+mapa_completo <- left_join(colonia, datos, by = "ID")
 
 ui <- dashboardPage(
   dashboardHeader(title = "DashBoard Col. Satelite"),
@@ -81,8 +91,57 @@ server <- function(input, output) {
   output$Grafica_Seleccionada <- renderText({
     paste("Tipo de gráfico seleccionado:", input$graph_option)
   })
-
-
+  # Renderizar el mapa de calor
+  output$mapa <- renderLeaflet({
+    mapa <- leaflet() %>%
+      addProviderTiles(providers$CartoDB.Positron)
+    
+    if (input$map_option == "Poblacion total") {
+      mapa <- mapa %>%
+        addPolygons(data = mapa_completo, 
+                    fillColor = ~colorNumeric("YlOrRd", mapa_completo$POBTOT)(POBTOT), 
+                    color = "black", 
+                    weight = 1, 
+                    opacity = 1, 
+                    fillOpacity = 0.7, 
+                    popup = ~paste("Población Total: ", POBTOT)) %>%
+        addLegend(pal = colorNumeric("YlOrRd", mapa_completo$POBTOT), 
+                  values = mapa_completo$POBTOT, 
+                  opacity = 0.7, 
+                  title = "Población Total",
+                  position = "bottomright")
+    } else if (input$map_option == "poblacion femenina") {
+      mapa <- mapa %>%
+        addPolygons(data = mapa_completo, 
+                    fillColor = ~colorNumeric("YlOrRd", mapa_completo$POBFEM)(POBFEM), 
+                    color = "black", 
+                    weight = 1, 
+                    opacity = 1, 
+                    fillOpacity = 0.7, 
+                    popup = ~paste("Población Femenina Total: ", POBFEM)) %>%
+        addLegend(pal = colorNumeric("YlOrRd", mapa_completo$POBFEM), 
+                  values = mapa_completo$POBFEM, 
+                  opacity = 0.7, 
+                  title = "Población Femenina Total",
+                  position = "bottomright")
+    } else if (input$map_option == "poblacion masculina") {
+      mapa <- mapa %>%
+        addPolygons(data = mapa_completo, 
+                    fillColor = ~colorNumeric("YlOrRd", mapa_completo$POBMAS)(POBMAS), 
+                    color = "black", 
+                    weight = 1, 
+                    opacity = 1, 
+                    fillOpacity = 0.7, 
+                    popup = ~paste("Población Masculina Total: ", POBMAS)) %>%
+        addLegend(pal = colorNumeric("YlOrRd", mapa_completo$POBMAS), 
+                  values = mapa_completo$POBMAS, 
+                  opacity = 0.7, 
+                  title = "Población Masculina Total",
+                  position = "bottomright")
+    }
+    
+    mapa
+  })
 
 }
 
